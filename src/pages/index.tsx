a04269e6479929a0e5fbd94232eade6dd91f70b1
unknown <fmmantelli@gmail.com>
2022-04-29 21:02:16 -0300

    Added SCSS to main page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,54 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Blog | Home</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <Link href="">
+            <a>
+              <h1>Como utilizar Hooks</h1>
+              <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
+              <div className={styles.box}>
+                <time>15 Mar 2021</time>
+                <h3>Joseph Oliveira</h3>
+              </div>
+            </a>
+          </Link>
+          <Link href="">
+            <a>
+              <h1>Criando um app CRA do zero</h1>
+              <h2>Tudo sobre como criar a sua primeira aplicação utilizando Create React App</h2>
+              <div className={styles.box}>
+                <time>19 Abr 2021</time>
+                <h3>Danilo Vieira</h3>
+              </div>
+            </a>
+          </Link>
+          <Link href="">
+            <a>
+              <h1>Como utilizar Hooks</h1>
+              <h2>Pensando em sincronização em vez de ciclos de vida.</h2>
+              <div className={styles.box}>
+                <time>15 Mar 2021</time>
+                <h3>Joseph Oliveira</h3>
+              </div>
+            </a>
+          </Link>
+        </div>
+
+        <div className={styles.containerMorePosts}>
+          <strong>Carregar mais posts</strong>
+        </div>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
